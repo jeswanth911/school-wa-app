dcf55942620a4f80b2bf6397b520afa7f68bbe23
jeswanth911 <feereminder911@gmail.com>
2025-07-14 02:09:49 +0530

    Create AgentDashboard.js

---
+++ src/pages/AgentDashboard.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function AgentDashboard() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>📦 Agent Dashboard</h2>
+      <ul>
+        <li>🏫 Assigned Schools List</li>
+        <li>📤 Help Send Messages</li>
+        <li>📊 View Logs</li>
+      </ul>
+    </div>
+  );
+}
