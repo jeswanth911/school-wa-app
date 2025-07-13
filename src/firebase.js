e382fdf385c6e335d2715af96ee2e6bb753e7e41
jeswanth911 <feereminder911@gmail.com>
2025-07-14 01:17:17 +0530

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDeBpQcUMO8ldhCmM3KmLUwn_w1FNSAcJg",
+  authDomain: "school-wa-auth.firebaseapp.com",
+  projectId: "school-wa-auth",
+  storageBucket: "school-wa-auth.firebasestorage.app",
+  messagingSenderId: "144565665526",
+  appId: "1:144565665526:web:73dc95ff86e3044d10b6d7"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
