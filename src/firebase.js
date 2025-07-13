0d1b944ae507e3261824bfd3207fd51c671f1e96
jeswanth911 <feereminder911@gmail.com>
2025-07-14 01:53:50 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,4 @@
+// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
@@ -6,12 +7,11 @@ const firebaseConfig = {
   apiKey: "AIzaSyDeBpQcUMO8ldhCmM3KmLUwn_w1FNSAcJg",
   authDomain: "school-wa-auth.firebaseapp.com",
   projectId: "school-wa-auth",
-  storageBucket: "school-wa-auth.firebasestorage.app",
+  storageBucket: "school-wa-auth.appspot.com",
   messagingSenderId: "144565665526",
   appId: "1:144565665526:web:73dc95ff86e3044d10b6d7"
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
 export const db = getFirestore(app);
