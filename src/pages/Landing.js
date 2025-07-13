3092c249c164afe54b548e6bbb730229307afca3
jeswanth911 <feereminder911@gmail.com>
2025-07-14 00:41:05 +0530

    Create Landing.js

---
+++ src/pages/Landing.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Landing() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>📱 School WhatsApp Reminder App</h1>
+      <p>Send fee and event reminders easily.</p>
+      <Link to="/login"><button>Login</button></Link>
+      <Link to="/register"><button>Register</button></Link>
+    </div>
+  );
+}
