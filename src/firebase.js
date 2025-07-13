1fd7999e6f4289b5d45f74e3e4834f6848e02e8a
jeswanth911 <feereminder911@gmail.com>
2025-07-14 03:09:29 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,17 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_DOMAIN",
+  apiKey: "AIzaSyDeBpQcUMO8ldhCmM3KmLUwn_w1FNSAcJg",
+  authDomain: "school-wa-auth.firebaseapp.com",
   projectId: "school-wa-auth",
-  storageBucket: "school-wa-auth.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  storageBucket: "school-wa-auth.firebasestorage.app",
+  messagingSenderId: "144565665526",
+  appId: "1:144565665526:web:73dc95ff86e3044d10b6d7"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
